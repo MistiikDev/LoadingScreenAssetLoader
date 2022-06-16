45710060454a8010183c735170690111835e0d6f
Mistiik <91028158+MistiikDev@users.noreply.github.com>
2022-06-16 15:49:27 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,16 +10,14 @@ local ContentProvider = game:GetService("ContentProvider") -- Service that allow
 for i, v in pairs(toLoad) do
   
     local total = #toLoad
-    local rapport = i/total -- Holding the division and keep tracking of the loop status
-    local prcnt = math.floor(rapport * 100) -- Rounding up the number to avoid getting like "10.56%" and mulitpliyng by 100 to get 50 and not 0.05
+    local rapport = i/total
+    local prcnt = math.floor(rapport * 100)
     
     bar:TweenSize(UDim2.new(rapport, 0, 1, 0), Enum.EasignDirection.Out, Enum.EasingStyle.Linear, .1, false) 
     
-    ContentProvider:PreLoadAsync({v}) -- Using "PreLoadAsync" function to actually load the current part. Putting v in brackets because the function takes a table
-    
-    loadingText.Text = prcnt.."%"
+    ContentProvider:PreLoadAsync({v}, function() 
+        loadingText.Text = "Currently loading asset : "..tostring(v.Name)..prcnt.."%"
+    end))    
 end
  
-loadingText.Text = "Loaded!" -- Loop ended, letting player know everything is loaded
- 
---// After that you can tween the GUI to go offscreen or just destroy it
+loadingText.Text = "Loaded!"
